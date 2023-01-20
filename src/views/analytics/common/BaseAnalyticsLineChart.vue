826a64de044526481391ecf8493584a3656df5cf
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-01-20 15:07:33 -0600

    Demo Chartjs

--- src/views/analytics/common/BaseAnalyticsLineChart.vue
+++ src/views/analytics/common/BaseAnalyticsLineChart.vue
@@ -22,11 +22,43 @@ export default {
         BaseLinearChart,
     },
 
+    data() {
+        return {
+            delayed: undefined,
+        };
+    },
+
     computed: {
         chartOptions() {
             return {
                 responsive: true,
-                maintainAspectRatio: true,
+                animation: {
+                    onComplete: () => {
+                        this.delayed = true;
+                    },
+                    delay: (context) => {
+                        let delay = 0;
+                        if (
+                            context.type === 'data' &&
+                            context.mode === 'default' &&
+                            !this.delayed
+                        ) {
+                            delay =
+                                context.dataIndex * 300 +
+                                context.datasetIndex * 100;
+                        }
+                        return delay;
+                    },
+                },
+                elements: {
+                    point: {
+                        radius: 0,
+                    },
+                    line: {
+                        borderJoinStyle: 'round',
+                        tension: 0.4,
+                    },
+                },
                 interaction: {
                     mode: 'index',
                     intersect: false,
@@ -42,6 +74,14 @@ export default {
         },
     },
 
+    created() {
+        /*this.chartData.datasets.forEach((element) => {
+            element.backgroundColor = this.poolColors(1);
+            element.borderColor = this.poolColors(1);
+            element.fill = true;
+        });*/
+    },
+
     methods: {
         $_chartFooter(tooltipItems) {
             return (
@@ -51,6 +91,21 @@ export default {
                     this.chartData.labels.indexOf(tooltipItems[0].label)
                 ]
             );
+        },
+
+        dynamicColors() {
+            var r = Math.floor(Math.random() * 255);
+            var g = Math.floor(Math.random() * 255);
+            var b = Math.floor(Math.random() * 255);
+            return 'rgba(' + r + ',' + g + ',' + b + ', 0.5)';
+        },
+
+        poolColors(a) {
+            var pool = [];
+            for (let i = 0; i < a; i++) {
+                pool.push(this.dynamicColors());
+            }
+            return pool;
         },
     },
 };
