a16399f84848033b74fb74d31a7aa2a45eb40e5a
joselynmonge-dev <117778122+joselynmonge-dev@users.noreply.github.com>
2023-03-31 09:26:41 -0600

    BTP-875  - Crear módulo de certificaciones
    Refactorización de código de la sección de certificaciones.

--- src/views/user/certification/CertificationsViewComponent.vue
+++ src/views/user/certification/CertificationsViewComponent.vue
@@ -5,6 +5,13 @@
  * @displayName CertificationsViewComponent
  *
  */
+import { mapGetters } from 'vuex';
+
+import baseConfigHelper from '@/helpers/baseConfigHelper';
+
+const BaseInputDataTable = () =>
+    import('@/components/core/forms/BaseInputDataTable');
+
 const BaseCardViewComponent = () =>
     import('@/components/core/cards/BaseCardViewComponent');
 
@@ -13,35 +20,235 @@ const CertificationFilter = () =>
         '@/views/user/certification/components/CertificationsFilterViewComponent'
     );
 
+const BaseCustomsButtonsGrid = () =>
+    import('@/components/core/grids/BaseCustomsButtonsGrid');
+
 export default {
     name: 'CertificationsViewComponent',
 
     components: {
+        BaseInputDataTable,
         BaseCardViewComponent,
         CertificationFilter,
+        BaseCustomsButtonsGrid,
     },
 
     data() {
         return {
             tab: null,
+            show: false,
+            organizacionId: undefined,
+            companyName: undefined,
+            componentKey: 0,
+            componentGrid: 0,
         };
     },
 
-    computed: {},
+    computed: {
+        ...mapGetters('authentication', ['user', 'buoId']),
+
+        companySetting() {
+            return {
+                endpoint: 'organizacion/findBy',
+                columns: [
+                    {
+                        text: 'Nombre',
+                        align: 'start',
+                        value: 'nombre',
+                        show: true,
+                    },
+                    {
+                        text: 'Nombre Contacto',
+                        align: 'start',
+                        value: 'nombreContacto',
+                        show: true,
+                    },
+                    {
+                        text: 'Correo Contacto',
+                        align: 'start',
+                        value: 'correoContacto',
+                        show: true,
+                    },
+                    {
+                        text: 'Token Colaborador',
+                        align: 'start',
+                        value: 'tokenUsuario',
+                        show: false,
+                    },
+                    {
+                        text: 'Colaboradores',
+                        align: 'end',
+                        value: 'totalUsuarios',
+                        show: false,
+                    },
+                    {
+                        text: 'Wallets Activas',
+                        align: 'end',
+                        value: 'walletsActivas',
+                        show: false,
+                    },
+                    {
+                        text: 'Certifica Inmediato',
+                        type: 'bool',
+                        align: 'center',
+                        value: 'certificaInmediato',
+                        show: false,
+                    },
+                    {
+                        text: 'Mostrar Puestos Genéricos',
+                        type: 'bool',
+                        align: 'center',
+                        value: 'mostrarPuestosGenericos',
+                        show: false,
+                    },
+                    {
+                        text: 'Demo',
+                        type: 'bool',
+                        align: 'center',
+                        value: 'esClienteDemo',
+                        show: true,
+                    },
+                    {
+                        text: 'Estado',
+                        align: 'center',
+                        value: 'nombreEstado',
+                        show: true,
+                    },
+                    {
+                        text: 'Industria',
+                        align: 'start',
+                        value: 'nombreIndustria',
+                        show: false,
+                    },
+                    {
+                        text: 'País',
+                        align: 'start',
+                        value: 'nombrePais',
+                        show: false,
+                    },
+                    {
+                        text: 'Ciudad',
+                        align: 'start',
+                        value: 'ciudad',
+                        show: false,
+                    },
+                    {
+                        text: 'Descripción',
+                        align: 'start',
+                        value: 'descripcion',
+                        show: false,
+                    },
+                ],
+                key: 'id',
+            };
+        },
+
+        certifying() {
+            return baseConfigHelper.$_statusCode.certifying;
+        },
+
+        certificate() {
+            return baseConfigHelper.$_statusCode.certificate;
+        },
+
+        rejected() {
+            return baseConfigHelper.$_statusCode.rejected;
+        },
+    },
+
+    methods: {
+        $_setParams() {
+            this.componentGrid++;
+        },
+
+        $_clean() {
+            this.organizacionId = this.user.companyId;
+            this.companyName = this.user.companyName;
+            this.componentGrid++;
+            this.componentKey++;
+            this.$_setParams();
+        },
+
+        $_showAdvFilter() {
+            this.show = !this.show;
+            if (this.show) {
+                if (this.user.companyId != this.buoId) {
+                    this.organizacionId = this.user.companyId;
+                }
+            }
+        },
+    },
 };
 </script>
 
 <template>
     <div>
         <BaseCardViewComponent title="Certificaciones">
-            <div slot="card-text">
-                <v-card flat>
+            <div slot="card-text" v-if="show">
+                <v-row dense>
+                    <v-col cols="12" md="6">
+                        <BaseForm
+                            :block="$vuetify.breakpoint.mobile"
+                            labelBtn="Buscar"
+                            :method="$_setParams"
+                        >
+                            <div slot="body">
+                                <v-row dense>
+                                    <v-col cols="12">
+                                        <p
+                                            class="BUO-Paragraph-Large-SemiBold grey700--text"
+                                        >
+                                            Seleccione la empresa
+                                        </p>
+                                        <BaseInputDataTable
+                                            v-if="user.companyId === buoId"
+                                            label="Empresa"
+                                            :editText="companyName"
+                                            :setting="companySetting"
+                                            v-model="organizacionId"
+                                            :validate="['requiered']"
+                                            :key="componentKey"
+                                        />
+                                    </v-col>
+                                </v-row>
+                            </div>
+                            <div slot="Beforebtns">
+                                <v-btn
+                                    class="ma-1 no-uppercase rounded-lg BUO-Paragraph-Small-SemiBold"
+                                    elevation="0"
+                                    large
+                                    outlined
+                                    color="primary"
+                                    @click="$_clean"
+                                >
+                                    Limpiar
+                                </v-btn>
+                            </div>
+                        </BaseForm>
+                    </v-col>
+                </v-row>
+            </div>
+            <div slot="body">
+                <v-card flat class="rounded-t-xl">
+                    <v-card-text>
+                        <v-layout justify-end>
+                            <BaseCustomsButtonsGrid
+                                label="Filtro Avanzado"
+                                :fnMethod="$_showAdvFilter"
+                                :outlined="!show"
+                                icon="mdi-filter-cog-outline"
+                                v-if="user.companyId === buoId"
+                            />
+                        </v-layout>
+                    </v-card-text>
+
                     <v-tabs
                         v-model="tab"
                         right
                         show-arrows
                         height="25"
                         class="pa-6 pt-5"
+                        flat
                     >
                         <v-tabs-slider color="transparent"></v-tabs-slider>
                         <v-tab class="rounded-pill no-uppercase"
@@ -58,19 +265,25 @@ export default {
                     <v-tabs-items v-model="tab" class="pa-5">
                         <v-tab-item>
                             <CertificationFilter
-                                :statusCode="4"
+                                :statusCode="certifying"
+                                :organizacionId="organizacionId"
+                                :key="componentGrid"
                             ></CertificationFilter>
                         </v-tab-item>
 
                         <v-tab-item>
                             <CertificationFilter
-                                :statusCode="5"
+                                :statusCode="certificate"
+                                :organizacionId="organizacionId"
+                                :key="componentGrid"
                             ></CertificationFilter>
                         </v-tab-item>
 
                         <v-tab-item>
                             <CertificationFilter
-                                :statusCode="6"
+                                :statusCode="rejected"
+                                :organizacionId="organizacionId"
+                                :key="componentGrid"
                             ></CertificationFilter>
                         </v-tab-item>
                     </v-tabs-items>
--- src/views/user/certification/components/CertificationsFilterViewComponent.vue
+++ src/views/user/certification/components/CertificationsFilterViewComponent.vue
@@ -5,13 +5,18 @@
  * @displayName CompetenceFilterViewComponent
  *
  */
+
 import { mapGetters } from 'vuex';
+
+import httpService from '@/services/axios/httpService';
+
+import baseConfigHelper from '@/helpers/baseConfigHelper';
+
+import baseLocalHelper from '@/helpers/baseLocalHelper.js';
 
 import baseSecurityHelper from '@/helpers/baseSecurityHelper';
 
 import baseNotificationsHelper from '@/helpers/baseNotificationsHelper';
-
-import baseLocalHelper from '@/helpers/baseLocalHelper.js';
 
 const BaseServerDataTable = () =>
     import('@/components/core/grids/BaseServerDataTable');
@@ -30,6 +35,11 @@ export default {
             type: Number,
             required: true,
         },
+
+        organizacionId: {
+            type: Number,
+            required: false,
+        },
     },
 
     components: {
@@ -42,8 +52,9 @@ export default {
             entity: this.$_Object(),
             search: 0,
             componentKey: 0,
-            bulkLoad: false,
             loading: false,
+            comment: undefined,
+            newStatusCode: undefined,
         };
     },
 
@@ -119,15 +130,10 @@ export default {
 
         extraParams() {
             let array = [];
-            if (this.user.companyId != this.buoId && !this.organizacionId) {
+            if (this.user.companyId != this.buoId) {
                 array.push({
                     name: 'organizacionId',
                     value: this.user.companyId,
-                });
-            } else if (this.organizacionId) {
-                array.push({
-                    name: 'organizacionId',
-                    value: this.organizacionId,
                 });
             } else if (this.entity.organizacionId) {
                 array.push({
@@ -143,6 +149,13 @@ export default {
                 });
             }
 
+            if (this.user.companyId != this.buoId && this.user.userId) {
+                array.push({
+                    name: 'userId',
+                    value: this.user.userId,
+                });
+            }
+
             return array.length > 0 ? array : undefined;
         },
 
@@ -155,31 +168,33 @@ export default {
         },
 
         showBottonApprove() {
-            return this.statusCode === 4 || this.statusCode === 6
+            return this.statusCode ===
+                baseConfigHelper.$_statusCode.certifying ||
+                this.statusCode === baseConfigHelper.$_statusCode.rejected
                 ? true
                 : false;
         },
 
         showBottonDecline() {
-            return this.statusCode === 4 || this.statusCode === 5
+            return this.statusCode ===
+                baseConfigHelper.$_statusCode.certifying ||
+                this.statusCode === baseConfigHelper.$_statusCode.certificate
                 ? true
                 : false;
         },
     },
 
-    watch: {
-        /**
-         * Actualizar
-         */
-        'entity.organizacionId': {
-            handler(newValue, oldValue) {
-                if (oldValue != newValue) {
-                    this.entity.categoriaId = undefined;
-                }
-            },
-            immediate: true,
-        },
+    created() {
+        if (
+            this.organizacionId != undefined &&
+            this.user.companyId != this.organizacionId
+        ) {
+            this.entity.organizacionId = this.organizacionId;
+        } else {
+            this.entity.organizacionId = this.user.companyId;
+        }
     },
+
     methods: {
         /**
          * Entity Object
@@ -188,20 +203,30 @@ export default {
             return {
                 id: undefined,
                 organizacionId: undefined,
-                nombreOrganizacion: undefined,
-                departamentoId: undefined,
-                nombreDepartamento: undefined,
                 personaId: undefined,
-                nombreEmpleado: undefined,
                 competenciaId: undefined,
-                definicionCompetencia: undefined,
                 cualificacionId: undefined,
-                definicionCualificacion: undefined,
                 usuarioId: undefined,
                 comentario: undefined,
                 estadoId: this.statusCode,
                 newEstadoId: undefined,
             };
+        },
+
+        /**
+         * Open a modal
+         */
+        $_open() {
+            this.$refs['popUp'].$_openModal();
+        },
+
+        /**
+         * Close a modal
+         */
+        $_close() {
+            if (this.$refs['popUp'].$_checkStatus()) {
+                this.$_open();
+            }
         },
 
         /**
@@ -212,55 +237,78 @@ export default {
         },
 
         /**
-         * Open a modal
+         * Verify that select some ability
          */
-        $_open() {
-            this.$refs['popUp'].$_openModal();
-        },
+        $_verifyAbility() {
+            const rows = this.$_GetRow().length;
 
-        $_setToUser() {
-            this.entity.usuarioId = this.user.userId;
-        },
-
-        $_changeStatusCompetence() {
-            const data = [];
-
-            switch (data.length) {
-                case 0:
+            switch (true) {
+                case rows == 0:
                     baseNotificationsHelper.Message(
                         true,
                         baseLocalHelper.$_MsgRowNotSelected
                     );
                     break;
-                case 1:
-                    this.$_GetRow().forEach((element) =>
-                        data.push(element[this.setting.key])
-                    );
-
-                    this.$_GetRow().length > 0
-                        ? (this.bulkLoad = true)
-                        : (this.entity = data);
-
+                case rows >= 1:
                     this.$_open();
                     break;
             }
         },
 
+        /**
+         * Set a certificate state for an ability
+         */
         $_approveAbility() {
-            this.$_changeStatusCompetence();
+            this.$_verifyAbility();
+            this.newStatusCode = baseConfigHelper.$_statusCode.certificate;
         },
 
-        $_declineAbility() {
-            this.$_changeStatusCompetence();
+        /**
+         * Set a rejected state for an ability
+         */
+        $_rejectedAbility() {
+            this.$_verifyAbility();
+            this.newStatusCode = baseConfigHelper.$_statusCode.rejected;
         },
 
-        $_sendToApi() {},
+        /**
+         * Set a information to ability or several
+         */
+        $_setAbility() {
+            let data = [];
+            this.$_GetRow().map((item) => {
+                item.usuarioId = this.user.userId;
+                item.newEstadoId = this.newStatusCode;
+                item.comentario = this.comment;
+                data.push(item);
+            });
+            this.entity = data;
+        },
+
+        $_sendToApi() {
+            this.loading = true;
+
+            this.$_setAbility();
+
+            httpService
+                .post(`empleadoCompetencia/updateStatusList`, this.entity)
+                .then((response) => {
+                    this.loading = false;
+                    if (response != undefined) {
+                        this.$refs.CertificationFilter.$_ParamsToAPI();
+                        this.comment = undefined;
+                        this.newStatusCode = undefined;
+                        this.componentKey++;
+                        this.$_close();
+                    }
+                });
+        },
     },
 };
 </script>
 
 <template>
-    <div slot="body">
+    <div>
         <BasePopUp
             ref="popUp"
             :maxWidth="$vuetify.breakpoint.mobile ? '100%' : '600'"
@@ -276,95 +324,23 @@ export default {
                             v-if="loading"
                             type="article, actions"
                         />
-                        <BaseForm :method="$_sendToApi" v-else>
+                        <BaseForm
+                            :method="$_sendToApi"
+                            :cancel="$_close"
+                            v-else
+                        >
                             <div slot="body">
                                 <v-row dense>
                                     <v-col cols="12">
-                                        <BaseInput
-                                            label="Empresa"
-                                            v-model.trim="
-                                                entity.nombreOrganizacion
-                                            "
-                                            :validate="['text']"
-                                            readonly
-                                            v-if="
-                                                this.user.companyId ===
-                                                    this.buoId &&
-                                                entity.nombreOrganizacion &&
-                                                !bulkLoad
-                                            "
-                                        />
-                                    </v-col>
-
-                                    <v-col cols="12">
-                                        <BaseInput
-                                            label="Área / Departamento"
-                                            v-model.trim="
-                                                entity.nombreDepartamento
-                                            "
-                                            :validate="['text']"
-                                            readonly
-                                            v-if="
-                                                entity.nombreDepartamento &&
-                                                !bulkLoad
-                                            "
-                                        />
-                                    </v-col>
-
-                                    <v-col cols="12">
-                                        <BaseInput
-                                            label="Nombre Empleado"
-                                            v-model.trim="entity.nombreEmpleado"
-                                            :validate="['text']"
-                                            v-if="
-                                                entity.nombreEmpleado &&
-                                                !bulkLoad
-                                            "
-                                            readonly
-                                        />
-                                    </v-col>
-
-                                    <v-col cols="12">
-                                        <BaseTextArea
-                                            label="Competencia"
-                                            v-model.trim="
-                                                entity.definicionCompetencia
-                                            "
-                                            :validate="['text']"
-                                            counter="1000"
-                                            readonly
-                                            v-if="
-                                                entity.definicionCompetencia &&
-                                                !bulkLoad
-                                            "
-                                        />
-                                    </v-col>
-
-                                    <v-col cols="12">
-                                        <BaseTextArea
-                                            label="Cualificación"
-                                            v-model.trim="
-                                                entity.definicionCualificacion
-                                            "
-                                            :validate="['text']"
-                                            counter="1000"
-                                            v-if="
-                                                entity.definicionCualificacion &&
-                                                !bulkLoad
-                                            "
-                                            readonly
-                                        />
-                                    </v-col>
-
-                                    <v-col cols="12">
                                         <BaseTextArea
                                             label="Comentario"
-                                            v-model.trim="entity.comentario"
+                                            v-model.trim="comment"
                                             :validate="['text']"
-                                            :max="1000"
+                                            :max="255"
                                             :min="1"
-                                            counter="1000"
+                                            counter="255"
                                             required
+                                            rows="10"
                                         />
                                     </v-col>
                                 </v-row>
@@ -374,26 +350,27 @@ export default {
                 </v-card>
             </div>
         </BasePopUp>
-        <BaseSkeletonLoader v-if="!user && !buoId" type="list-item" />
         <BaseServerDataTable
             ref="CertificationFilter"
+            :key="componentKey"
             :setting="setting"
             :extraParams="extraParams"
-            v-else
         >
             <div slot="btns">
                 <BaseCustomsButtonsGrid
                     v-if="permission && showBottonApprove"
                     label="Aprobar"
                     :fnMethod="$_approveAbility"
-                    icon="mdi-check-outline"
+                    icon="mdi-thumb-up-outline"
+                    color="greenB900"
                 />
 
                 <BaseCustomsButtonsGrid
                     v-if="permission && showBottonDecline"
                     label="Rechazar"
-                    :fnMethod="$_declineAbility"
-                    icon="mdi-delete-outline"
+                    :fnMethod="$_rejectedAbility"
+                    icon="mdi-thumb-down-outline"
+                    color="redError900"
                 />
             </div>
         </BaseServerDataTable>
