7490581bb57a3d7c17a29c0714a85a6a32b3f5d0
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-04-12 22:20:58 -0600

    BTP-912   Crear UI para asignación masiva de habilidades

---
+++ src/views/assignment/components/NewAbilityViewComponent.vue
@@ -0,0 +1,65 @@
+<script>
+/**
+ * Descripción: Pantalla Busqueda de talento
+ *
+ * @displayName FilterViewComponent
+ *
+ */
+
+const BaseCustomsButtonsGrid = () =>
+    import('@/components/core/grids/BaseCustomsButtonsGrid');
+
+export default {
+    name: 'NewAbilityViewComponent',
+
+    props: {
+        entity: {
+            type: Object,
+            requiered: true,
+        },
+
+        fn: {
+            type: Function,
+            requiered: true,
+        },
+    },
+
+    components: {
+        BaseCustomsButtonsGrid,
+    },
+
+    methods: {
+        $_sendToApi() {
+            console.log('hola');
+        },
+
+        $_tryOpen() {
+            this.fn();
+            if (this.entity && this.entity?.selected?.userList.length > 0) {
+                this.$_open();
+            }
+        },
+
+        $_open() {
+            this.$refs['popUp'].$_openModal();
+        },
+    },
+};
+</script>
+
+<template>
+    <div>
+        <BasePopUp
+            ref="popUp"
+            :maxWidth="$vuetify.breakpoint.mobile ? '100%' : '600'"
+            scrollable
+        >
+            <div slot="Content">hola</div>
+        </BasePopUp>
+        <BaseCustomsButtonsGrid
+            label="Crear nueva habilidad"
+            :fnMethod="$_tryOpen"
+            icon="mdi-shield"
+        />
+    </div>
+</template>
