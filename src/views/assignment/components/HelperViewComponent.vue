2b5ca9b13eed057df3948acbda6666b0c5b3afa1
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-05-08 11:13:23 -0600

    Update HelperViewComponent.vue

--- src/views/assignment/components/HelperViewComponent.vue
+++ src/views/assignment/components/HelperViewComponent.vue
@@ -37,32 +37,79 @@ export default {
                             lo siguiente:
                         </div>
                         <div>
-                            <v-expansion-panels multiple flat>
+                            <v-expansion-panels flat>
                                 <v-expansion-panel>
                                     <v-expansion-panel-header
-                                        >Crear nuevo
-                                        Indicador</v-expansion-panel-header
-                                    >
+                                        ><span
+                                            class="BUO-Paragraph-Large blue900--text"
+                                            >Crear nuevo Indicador</span
+                                        >
+                                    </v-expansion-panel-header>
                                     <v-expansion-panel-content>
-                                        Hola
+                                        <div
+                                            class="BUO-Label-Small grey500--text"
+                                        >
+                                            <p>
+                                                El indicador debe tener un
+                                                nombre único para evitar
+                                                confusiones entre indicadores.
+                                            </p>
+                                            <p>
+                                                El proceso crea un indicador
+                                                nuevo y no verifica si ya existe
+                                                otro con el mismo nombre.
+                                            </p>
+                                        </div>
                                     </v-expansion-panel-content>
                                 </v-expansion-panel>
                                 <v-expansion-panel>
                                     <v-expansion-panel-header
-                                        >Asignar Indicador
-                                        existente</v-expansion-panel-header
+                                        ><span
+                                            class="BUO-Paragraph-Large blue900--text"
+                                            >Asignar Indicador existente
+                                        </span></v-expansion-panel-header
                                     >
                                     <v-expansion-panel-content>
-                                        Hola
+                                        <div
+                                            class="BUO-Label-Small grey500--text"
+                                        >
+                                            Si el indicador ya había sido
+                                            asignado a la persona anteriormente,
+                                            el proceso va a actualizar la fecha
+                                            y el estado seleccionado, es decir,
+                                            el indicador se actualiza con la
+                                            fecha y el estado actual.
+                                        </div>
                                     </v-expansion-panel-content>
                                 </v-expansion-panel>
                                 <v-expansion-panel>
                                     <v-expansion-panel-header
-                                        >Asignación
-                                        masiva</v-expansion-panel-header
+                                        ><span
+                                            class="BUO-Paragraph-Large blue900--text"
+                                            >Asignación masiva</span
+                                        ></v-expansion-panel-header
                                     >
                                     <v-expansion-panel-content>
-                                        Hola
+                                        <div
+                                            class="BUO-Label-Small grey500--text"
+                                        >
+                                            <p>
+                                                Debe seleccionar si desea crear
+                                                un indicador o asignar uno
+                                                existente. Esta opción aplica
+                                                para TODAS las personas.
+                                            </p>
+                                            <p>
+                                                Si es administradora de empresa,
+                                                aplica el proceso a TODAS las
+                                                personas de la empresa.
+                                            </p>
+                                            <p>
+                                                Si es administradora de
+                                                departamento aplica a TODAS las
+                                                personas bajo su supervisión.
+                                            </p>
+                                        </div>
                                     </v-expansion-panel-content>
                                 </v-expansion-panel>
                             </v-expansion-panels>
