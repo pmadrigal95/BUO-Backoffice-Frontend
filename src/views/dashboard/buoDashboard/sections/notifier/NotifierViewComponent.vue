6c1df1bced423373ca7172cfd430c47d83a592e5
joselynmonge-dev <117778122+joselynmonge-dev@users.noreply.github.com>
2024-01-22 15:20:38 -0600

    Ajuste de diseño en componente de notificación para el nuevo dasboard

--- src/views/dashboard/buoDashboard/sections/notifier/NotifierViewComponent.vue
+++ src/views/dashboard/buoDashboard/sections/notifier/NotifierViewComponent.vue
@@ -55,37 +55,63 @@ export default {
         <div slot="Content">
             <v-card flat height="100%" width="100%">
                 <v-card-title
-                    class="BUO-Heading-Small"
+                    class="BUO-Heading-XSmall"
                     :class="[app ? 'blueProgress600--text' : 'blue900--text']"
                     >Bienvenido a Buo Dashboard</v-card-title
                 >
                 <v-card-text
-                    class="BUO-Paragraph-Large"
+                    class="BUO-Paragraph-Small"
                     :class="[app ? 'white--text' : 'grey700--text']"
                 >
                     <!--TODO: Poner bonito-->
                     <section class="pb-4">
-                        Para garantizar una experiencia visual óptima en todos
-                        sus dispositivos, hemos diseñado cuidadosamente los
-                        gráficos de manera responsiva. Esto significa que los
-                        gráficos se adaptarán de forma inteligente a la
-                        resolución de la pantalla de su laptop, tablet y
-                        celular. En su laptop, disfrutará de una presentación
-                        detallada y expansiva, aprovechando al máximo el espacio
-                        disponible. En una tablet, la visualización se ajustará
-                        de manera fluida para ofrecer una experiencia táctil
-                        intuitiva y fácil de navegar. Por último, en su celular,
-                        los gráficos se optimizarán para una visualización clara
-                        y legible, asegurando que la información es fácilmente
-                        accesible incluso en pantallas más pequeñas. Este
-                        enfoque garantiza coherencia y calidad visual en todos
-                        sus dispositivos, proporcionando una experiencia de
-                        usuario consistente y atractiva.
+                        <p>
+                            Para garantizar una experiencia visual óptima en
+                            todos sus dispositivos, hemos diseñado
+                            cuidadosamente los gráficos de manera responsiva.
+                            Esto significa que los gráficos se adaptarán de
+                            forma inteligente a la resolución de la pantalla de
+                            su laptop, tablet y celular.
+                        </p>
+
+                        <p>
+                            En su
+                            <span class="BUO-Paragraph-Small-SemiBold"
+                                >laptop</span
+                            >, disfrutará de una presentación detallada y
+                            expansiva, aprovechando al máximo el espacio
+                            disponible.
+                        </p>
+
+                        <p>
+                            En una
+                            <span class="BUO-Paragraph-Small-SemiBold"
+                                >tablet</span
+                            >, la visualización se ajustará de manera fluida
+                            para ofrecer una experiencia táctil intuitiva y
+                            fácil de navegar.
+                        </p>
+
+                        <p>
+                            Por último, en su
+                            <span class="BUO-Paragraph-Small-SemiBold"
+                                >celular</span
+                            >, los gráficos se optimizarán para una
+                            visualización clara y legible, asegurando que la
+                            información es fácilmente accesible incluso en
+                            pantallas más pequeñas.
+                        </p>
+
+                        <p>
+                            Este enfoque garantiza coherencia y calidad visual
+                            en todos sus dispositivos, proporcionando una
+                            experiencia de usuario consistente y atractiva.
+                        </p>
                     </section>
                 </v-card-text>
                 <v-card-actions>
                     <v-btn
-                        class="mt-5 no-uppercase rounded-lg BUO-Paragraph-Small-SemiBold"
+                        class="no-uppercase rounded-lg BUO-Paragraph-Small-SemiBold"
                         elevation="0"
                         color="primary"
                         block
