dcfcfb16972eb7a725c9dbfe60de743487b24688
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2024-03-14 17:57:06 -0600

    Update BaseCustomCardViewComponent.vue
    BaseCustomCardViewComponent

---
+++ src/views/dashboard/components/shared/card/BaseCustomCardViewComponent.vue
@@ -0,0 +1,210 @@
+<script>
+/**
+ * Descripción: Pantalla Card que maneja la cantidad de columnas que necesite
+ *
+ * @displayName BaseCustomCardViewComponent
+ *
+ */
+
+export default {
+    name: 'BaseCustomCardViewComponent',
+
+    props: {
+        card: {
+            type: Object,
+            default: () => ({
+                width: '100%',
+                height: '100%',
+                color: undefined,
+                borderColor: undefined,
+                content: 'pa-0',
+                elevation: 0,
+                display: {
+                    flex: 'flex-row',
+                    justify: undefined,
+                    align: 'align-center',
+                },
+            }),
+        },
+
+        icon: {
+            type: Object,
+            default: () => ({
+                src: undefined,
+                size: {
+                    width: '17px',
+                    height: '19px',
+                },
+                container: {
+                    width: '49px',
+                    height: '39px',
+                },
+                color: undefined,
+                backgroundColor: undefined,
+            }),
+        },
+
+        header: {
+            type: Object,
+            default: () => ({
+                title: undefined,
+                color: 'aidMint500--text',
+                style: 'BUO-Label-Small',
+                content: 'pt-4',
+            }),
+        },
+    },
+
+    computed: {
+        cardSetUp() {
+            return {
+                width: this.card?.width || '100%',
+                height: this.card?.height || '100%',
+                color: this.card?.color,
+                elevation: this.card?.elevation || 0,
+                borderColor: this.card?.borderColor || this.card?.color,
+                content: this.card?.content || 'pa-0',
+                display: {
+                    flex: this.card?.display?.flex || 'flex-row',
+                    justify: this.card?.display?.justify,
+                    align: this.card?.display?.align || 'align-center',
+                },
+            };
+        },
+
+        cardDisplaySetUp() {
+            return {
+                ...this.this.cardSetUp?.display,
+            };
+        },
+
+        iconSetUp() {
+            return {
+                src: this.icon?.src,
+                size: {
+                    width: this.icon?.size?.width || '17px',
+                    height: this.icon?.size?.height || '19px',
+                },
+                container: {
+                    width: this.icon?.container?.width || '49px',
+                    height: this.icon?.container?.height || '39px',
+                },
+                color: this.icon?.color,
+                backgroundColor: this.icon?.backgroundColor || this.icon?.color,
+            };
+        },
+
+        iconSizeSetUp() {
+            return {
+                ...this.iconSetUp?.size,
+            };
+        },
+
+        iconContainerSetUp() {
+            return {
+                ...this.iconSetUp?.container,
+            };
+        },
+
+        headerSetUp() {
+            return {
+                title: this.header?.title,
+                color: this.header?.color || 'aidMint500--text',
+                style: this.header?.style || 'BUO-Label-Small',
+                content: this.header?.content || 'pt-4',
+            };
+        },
+
+        headerStyleSetUp() {
+            return `${this.headerSetUp?.color} ${this.headerSetUp?.style} ${this.headerSetUp?.content}`;
+        },
+
+        isvalidIcon() {
+            return this.iconSetUp?.src.includes('mdi-');
+        },
+
+        isValidUrl() {
+            return true;
+        },
+    },
+};
+</script>
+
+<template>
+    <v-sheet
+        outlined
+        rounded="lg"
+        :color="cardSetUp.color"
+        :width="cardSetUp.width"
+        :height="cardSetUp.height"
+    >
+        <v-card
+            class="rounded-lg"
+            :color="cardSetUp.color"
+            :width="cardSetUp.width"
+            :height="cardSetUp.height"
+            :elevation="cardSetUp.elevation"
+        >
+            <v-card-text :class="cardSetUp.content">
+                <section v-if="icon.src">
+                    <v-card
+                        :width="iconContainerSetUp.width"
+                        :height="iconContainerSetUp.height"
+                        :color="iconSetUp.backgroundColor"
+                        class="d-flex justify-start align-center rounded-xxl img-position"
+                    >
+                        <v-card-text>
+                            <section class="d-flex flex-row align-center">
+                                <section v-if="isvalidIcon">
+                                    <v-icon
+                                        class="icon-position"
+                                        :color="iconSetUp.color"
+                                        large
+                                    >
+                                        {{ iconSetUp.src }}
+                                    </v-icon>
+                                </section>
+                                <section v-else-if="isValidUrl">
+                                    <v-img
+                                        contain
+                                        :width="iconSizeSetUp.width"
+                                        :height="iconSizeSetUp.height"
+                                        :src="iconSetUp.src"
+                                        :lazy-src="iconSetUp.src"
+                                    />
+                                </section>
+                                <p
+                                    v-if="headerSetUp.title"
+                                    :class="headerStyleSetUp"
+                                >
+                                    {{ headerSetUp.title }}
+                                </p>
+                            </section>
+                        </v-card-text>
+                    </v-card>
+                </section>
+
+                <section
+                    class="d-flex"
+                    :class="`${cardDisplaySetUp.flex} ${cardDisplaySetUp.justify} ${cardDisplaySetUp.align}`"
+                >
+                    <slot name="column"></slot>
+                </section>
+            </v-card-text>
+        </v-card>
+    </v-sheet>
+</template>
+
+<style scoped>
+.img-position {
+    top: -20px;
+    left: 15px;
+    position: relative !important;
+}
+
+.icon-position {
+    top: -20px;
+    left: 10px;
+    position: relative !important;
+}
+</style>
