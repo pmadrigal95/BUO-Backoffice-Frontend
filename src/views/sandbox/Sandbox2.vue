f53ef5245d53485419b34dcd15b9a66552e03231
joselyncode <joselyncode@gmail.com>
2024-05-07 16:07:51 -0600

    Add statistical home component

--- src/views/sandbox/Sandbox2.vue
+++ src/views/sandbox/Sandbox2.vue
@@ -1,11 +1,40 @@
 <script>
 const PsychometricProfileViewComponent = () =>
     import('@/views/sandbox/components/PsychometricProfileViewComponent');
+
+const StatisticalHomeCardsViewComponent = () =>
+    import('@/views/sandbox/components/StatisticalHomeCardsViewComponent');
+
 export default {
     name: 'Sandbox2',
 
     components: {
         PsychometricProfileViewComponent,
+        StatisticalHomeCardsViewComponent,
+    },
+
+    computed: {
+        statisticalList() {
+            return [
+                {
+                    numericPercentage: '350',
+                    title: 'Empleados',
+                },
+                {
+                    color: 'greenB600',
+                    direction: 'down',
+                    numericPercentage: '37.5',
+                    title: 'Paridad de Género',
+                },
+                {
+                    color: 'greenB600',
+                    description: '+5 vs el mes anterior',
+                    direction: 'down',
+                    numericPercentage: '3,5',
+                    title: 'Antigüedad (años)',
+                },
+            ];
+        },
     },
 };
 </script>
@@ -13,6 +42,13 @@ export default {
 <template>
     <div>
         <v-row>
+            <v-col cols="12">
+                <StatisticalHomeCardsViewComponent
+                    :chartData="statisticalList"
+                    height="171"
+                    width="297"
+                />
+            </v-col>
             <v-col cols="12">
                 <PsychometricProfileViewComponent
                     imageURL="https://buo-resources.s3.us-east-2.amazonaws.com/hotcakes_simple/SimpleHotcakeCautivador.svg"
